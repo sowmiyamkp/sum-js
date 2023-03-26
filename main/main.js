180ae4228c83c965a5629bfcea35d338baeca2ae
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-25 23:29:48 -0700

    add sum-js

---
+++ main/main.js
@@ -0,0 +1,39 @@
+//sum 
+
+var array=[1,2,3,4,5];
+var sum=0;
+for(i=0;i<array.length;i++)
+{
+	sum=sum+array[i];
+}
+console.log(sum);
+
+//function use sum
+
+var array=[9,10,11,12,13];
+var sum=0;
+function sumNum(array)
+{
+	for(i=0;i<array.length;i++)
+	{
+		sum+=array[i];
+	}
+	return sum;
+}
+console.log(sumNum(array));
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+
+{
+	console.log((a*b));
+}
+
+var f="fab";h=5;g=5;
+(h+g)||(f+h+g)
+{
+	console.log((h+g)||(f+h+g))
+}
+
+
+
